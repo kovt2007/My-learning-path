89813b7eda646e776d69db00ae0e7c6e301c279e
Ivan <60852892+kovt2007@users.noreply.github.com>
2024-09-26 21:19:31 +0200

    Add files via upload

---
+++ Collector of information about a person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace HW
+{
+    class Training
+    {
+        static void Main()
+        {
+            string b;
+            string a;
+
+            Console.WriteLine("Write your name");
+            a = Console.ReadLine();
+            Console.WriteLine("Write your age");
+            b = Console.ReadLine();
+            Console.WriteLine("Your name - " + a);
+            Console.WriteLine("Your age - " + b);
+            Console.ReadKey();
+        }
+
+    }
+}
