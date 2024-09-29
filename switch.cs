fd964b10fb6d5f140776fd01522e60763c165297
Ivan <60852892+kovt2007@users.noreply.github.com>
2024-09-29 19:02:09 +0200

    Here I studied the design of the switch

---
+++ switch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+internal class Program
+
+
+{
+    private static void Main(string[] args)
+    {
+        char a = Convert.ToChar(Console.ReadLine());
+
+        switch (a)
+        {
+            case 'a':
+                Console.WriteLine("You write a");
+                break;
+            default:
+                Console.WriteLine("I dont know this");
+                break;
+
+        }
+
+    }
+}
